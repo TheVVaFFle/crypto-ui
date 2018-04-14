0a109248534c58ae9e6a2192477c22c8eb2e7fe5
TheVVaFFle <frontenddevemail@gmail.com>
2018-04-14 15:28:56 -0500

    Details view transition effect.

--- client/components/currencyDetailsView/currencyDetailsView.jsx
+++ client/components/currencyDetailsView/currencyDetailsView.jsx
@@ -1,42 +1,58 @@
-import React from 'react'
+import React, {Component} from 'react'
+import classNames from 'classnames'
 
-const CurrencyDetailsView = ({
-  details
-}) => {
-  let currencyDetails = null
-  if(details){
-    currencyDetails = (
-      <div id="currency-details-view">
-        <div id="currency-details-header">
-          <div id="currency-details-icon">
-            <div className="icon-image" style={{backgroundImage: `url(/img/icons/color/${details.abbrev.toLowerCase()}.svg)`}}/>
-          </div>
-          <div id="currency-details-name">
-            <h1>{details.name.toUpperCase()}</h1>
-          </div>
-          <div id="currency-details-abbrev">
-            <h1>({details.abbrev.toUpperCase()})</h1>
-          </div>
-          <div id="currency-details-price">
-            <div className="value">
-              <h1>${details.price}</h1>
+class CurrencyDetailsView extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      isTransitioning: false
+    }
+  }
+  componentDidUpdate(prevProps, prevState){
+    if(!prevState.isTransitioning && !this.state.isTransitioning){
+      this.setState({isTransitioning: true})
+      setTimeout(() => this.setState({isTransitioning: false}), 500)
+    }
+  }
+  render(){
+    const {details} = this.props
+    const detailsViewClasses = classNames({"is-transitioning": this.state.isTransitioning})
+    let currencyDetails = null
+    
+    if(details){
+      currencyDetails = (
+        <div id="currency-details-view" className={detailsViewClasses}>
+          <div id="currency-details-header">
+            <div id="currency-details-icon">
+              <div className="icon-image" style={{backgroundImage: `url(/img/icons/color/${details.abbrev.toLowerCase()}.svg)`}}/>
             </div>
-            <div className="label">
-              <h1>PRICE</h1>
+            <div id="currency-details-name">
+              <h1>{details.name.toUpperCase()}</h1>
             </div>
+            <div id="currency-details-abbrev">
+              <h1>({details.abbrev.toUpperCase()})</h1>
+            </div>
+            <div id="currency-details-price">
+              <div className="value">
+                <h1>${details.price}</h1>
+              </div>
+              <div className="label">
+                <h1>PRICE</h1>
+              </div>
+            </div>
+          </div>
+          <div id="currency-details-body">
+
           </div>
         </div>
-        <div id="currency-details-body">
-
-        </div>
+      )
+    }
+    return(
+      <div id="currency-details-view-wrapper">
+        {currencyDetails}
       </div>
     )
   }
-  return(
-    <div id="currency-details-view-wrapper">
-      {currencyDetails}
-    </div>
-  )
 }
 
 export {
